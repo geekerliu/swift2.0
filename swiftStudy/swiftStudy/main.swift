fbfcb96700544751c47a556d5599a8d3a980acd8
LiuWei <987860319@qq.com>
2015-10-08 12:13:22 +0800

    chapter2
    基本运算符

--- swiftStudy/swiftStudy/main.swift
+++ swiftStudy/swiftStudy/main.swift
@@ -44,7 +44,29 @@ func chapter1() {
     // assertion
 }
 
+// 基本运算符
 func chapter2() {
+    //1.赋值运算符
+//    let (x, y) = (1, 2)
+//    if x = y { //错，与 C 语言和 Objective-C 不同,Swift 的赋值操作并不返回任何值
+//        
+//    }
+    let a = 8 % 2.5//可对浮点数求余
+    print(a)
+    
+    //Nil Coalescing Operator ??
+    //空合运算符( a ?? b )将对可选类型 a 进行空判断,如果 a 包含一个值就进行解封,否则就返回一个默认值 b,必须满足下面两个条件
+    //1.表达式 a 必须是Optional类型
+    //2.默认值 b 的类型必须要和 a 存储值的类型保持一致
+    let defaultColorName = "red"
+    var userDefinedColorName:String? //默认值为nil
+    userDefinedColorName = "green"
+    var colorNameToUse = userDefinedColorName ?? defaultColorName
+    print(colorNameToUse)
+    
+    //区间运算符 
+//    a...b 闭区间
+//    a..<b 半开半闭区间
     
 }
 
